6bef01f7a5f268a354f81020fd7227a7cc7912bc
wjarocinski <wojciech.jarocinski@atos.net>
2019-06-19 12:00:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/scss/bootstrap.scss';
+import './overrides.scss';
+import "./App.css";
+
+
+
+//ADD TODO
+function ToDoForm({ addTodo }) {
+  const [value, setValue] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' className='input' placeholder='Add Todo...' value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+
+
+
+
+
+
+
+//COMPONENTS
+
+function Todo({ todo, index, completeTodo, deleteTodo }) {
+  return (
+    <div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} className='todo'>
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => deleteTodo(index)}> Delete </button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn React',
+      isCompleted: false
+    },
+    {
+      text: 'Learn Angular',
+      isCompleted: false
+    },
+    {
+      text: 'Learn Vue',
+      isCompleted: false
+    },
+    {
+      text: 'Learn about GraphQL',
+      isCompleted: false
+    },
+    {
+      text: 'Learn about Redux',
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo} />
+        ))}
+        <ToDoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
